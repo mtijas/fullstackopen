bd1a3f335ab712f668549f96e0c62b1d7522e7cc
Markus Ijäs <markus.ijas@outlook.com>
2023-01-06 14:28:22 +0200

    WIP part1c

--- work-in-progress/part1/src/App.js
+++ work-in-progress/part1/src/App.js
@@ -1,19 +1,24 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
-  );
-};
+import { useState } from "react";
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter+1);
+  const decreaseByOne = () => setCounter(counter-1);
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='George' />
-      <Hello name='Daisy' />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
     </div>
-  );
+  )
 };
 
 export default App;
