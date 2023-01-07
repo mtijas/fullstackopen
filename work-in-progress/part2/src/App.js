50f9c15a72bf6e435c11dd60a3b12c96c6e26eea
Markus Ijäs <markus.ijas@outlook.com>
2023-01-07 12:26:29 +0200

    Adds WIP for part2

---
+++ work-in-progress/part2/src/App.js
@@ -0,0 +1,16 @@
+import Note from './components/Note'
+
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note note={note} key={note.id} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
