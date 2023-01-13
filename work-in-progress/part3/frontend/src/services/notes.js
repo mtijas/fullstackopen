3e9b12bee81f9d69d125366c496941cfa80c0a48
Markus Ijäs <markus.ijas@outlook.com>
2023-01-13 17:41:56 +0200

    Adds WIP notes backend and frontend

--- work-in-progress/part3/frontend/src/services/notes.js
+++ work-in-progress/part3/frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = process.env.REACT_APP_BACKEND_BASEURL || '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
