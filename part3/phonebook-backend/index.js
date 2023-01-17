9fca1083fd1fa31a6c2dab79b44de329e9302f4e
Markus Ijäs <markus.ijas@outlook.com>
2023-01-17 20:54:52 +0200

    Updates part3 with e3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
-morgan.token("postdata", (request, response) => {
+morgan.token("postdata", (request) => {
   if (request.method === "POST") {
     return JSON.stringify(request.body);
   }
@@ -54,7 +54,7 @@ app.get("/api/persons/:id", (request, response, next) => {
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
